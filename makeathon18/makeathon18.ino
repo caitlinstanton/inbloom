74b5dc40db66122bf69d250edb4877a4792dc6d1
Mishcat <mi243@cornell.edu>
2018-02-24 20:51:41 -0500

    added flashing lights

--- makeathon18/makeathon18.ino
+++ makeathon18/makeathon18.ino
@@ -1,5 +1,16 @@
+#include <Wire.h>
+#include "rgb_lcd.h"
+rgb_lcd lcd;
 
 #include <Servo.h>
+
+#include <Arduino.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+
+
 
 
 unsigned long StartTime = millis();
@@ -18,64 +29,99 @@ int servo3_on=0;
 int button1;
 int button2;
 int button3;
-//int button4;
+
 
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  Serial.begin(9600);
-  int servo1_on=0;
-int servo2_on=0;
-int servo3_on=0;
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(3, INPUT);
-  pinMode(4, OUTPUT);
+    lcd.begin(16, 2);
+    lcd.setRGB(255, 255, 255);
+    Serial.begin(9600);
 
-//buzzer
-pinMode(2, OUTPUT);
+    
+   
+    int servo1_on=0;
+    int servo2_on=0;
+    int servo3_on=0;
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(3, INPUT);
+
+
+    //buzzer
+    pinMode(2, OUTPUT);
   
-  myservo1.attach(servopin1);
-  myservo2.attach(servopin2);
-  myservo3.attach(servopin3);
- 
+    myservo1.attach(servopin1);
+    myservo2.attach(servopin2);
+    myservo3.attach(servopin3);
+
+  
 }
 
 // the loop function runs over and over again forever
 void loop() {
- unsigned long CurrentTime = millis();
- unsigned long ElapsedTime = (CurrentTime - StartTime)/1000;
 
+  lcd.noDisplay();
+  lcd.display();
+  unsigned long CurrentTime = millis();
+  unsigned long ElapsedTime = (CurrentTime - StartTime)/1000;
   int touch_sensor = digitalRead(3);
 
   if (touch_sensor==LOW){
+    lcd.clear();
+    lcd.noDisplay();
+    lcd.display();
     StartTime = CurrentTime;
     ElapsedTime=0;
-    
+    lcd.setCursor(0, 1); 
   }
- else if (ElapsedTime >= 15 && ElapsedTime <=16 && touch_sensor==HIGH){
+  
+ else if (ElapsedTime >= 15 && ElapsedTime <=17 && touch_sensor==HIGH){
+    lcd.noDisplay();
+    lcd.display();
+    lcd.setRGB(255, 255, 0);
     tone(2, 1000,1000);
-   digitalWrite(4, LOW);
+    int touch_sensor = digitalRead(3);
+    lcd.setCursor(0, 1);
+    lcd.print("Usage time: 15s!");
+
  }
+
  else if (ElapsedTime >= 30 && ElapsedTime <=32 && touch_sensor==HIGH){
     tone(2, 2000,2000);
-    digitalWrite(4, LOW);
+    lcd.noDisplay();
+    lcd.display();
+    lcd.setRGB(0, 255, 0);
+    delay(25);
+    lcd.setCursor(0, 1);
+    lcd.print("Usage time: 30s!");
  }
+
  else if (ElapsedTime >= 60 && ElapsedTime <=63 && touch_sensor==HIGH){
-    tone(2, 3000,3000);
-    digitalWrite(4, LOW);
+     tone(2, 3000,3000);
+     delay(100);
+     lcd.setRGB(255, 0, 0);
+     lcd.noDisplay();
+     delay(25);
+     lcd.display();
+     lcd.setCursor(0, 1);
+     lcd.print("Usage time: 60s!");
+     delay(100);
+     lcd.setRGB(255, 255, 255);
  }
+    
  else{
-   digitalWrite(4, HIGH);
+    lcd.setRGB(255, 255, 255);
+    lcd.noDisplay();
+    lcd.display();
+    lcd.setCursor(0, 1);
+    lcd.print("                 ");
+
  }
-    // myservo.write(0);
+
   button1=digitalRead(11);
   button2=digitalRead(12);
   button3=digitalRead(13);
-
-
- // button4=digitalRead(10);
-  
   
   if (button1){
     servo1_on=1;
@@ -98,14 +144,4 @@ void loop() {
     myservo3.write(90);
     servo3_on=2;
   }
-//    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-                     // wait for a second
-  //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  //delay(1000);    
-  
-  //else{
-    //  myservo1.write(0);
-   //  digitalWrite(led, LOW); 
-     // wait for a second
- //}
 }
